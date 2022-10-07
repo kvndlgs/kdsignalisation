420b3788fb1525195ed650924eb67c6917c60da3
kvndlgs <es2thekay@gmail.com>
2022-10-07 15:15:28 -0500

    uh

--- components/page-title.js
+++ components/page-title.js
@@ -2,7 +2,7 @@ export default function PageTitle({children}){
     return (
         <div className='w-full px-2 h-auto flex items-center justify-center pt-10 pb-6 md:pt-20 md:pb-8'>
         <div className='w-2/8 text-center'>
-        <h1 className='md:text-5xl text-3xl text-darkgray font-one tracking-tight leading-none'>
+        <h1 className='md:text-6xl text-4xl text-darkgray font-one tracking-tight leading-none'>
             {children}
         </h1>
         </div>
